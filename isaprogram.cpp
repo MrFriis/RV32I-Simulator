8673b3102d7617131bd4e0aa7b354677b86f7c1d
MrFriis <mrsf@student.dtu.dk>
2018-11-29 13:10:55 +0100

    Comments

--- isaprogram.cpp
+++ isaprogram.cpp
@@ -20,7 +20,7 @@ ISAProgram::ISAProgram(){
     registers_[i] = 0;
   }
 
-  //REMOVE
+  //Preallocating memory.
   memory_ = new unsigned char[0x100010];
   capacity_ = 0x100010;
 
@@ -39,6 +39,10 @@ ISAProgram::~ISAProgram(){
   }
 }
 
+//Function that read instructions from a binary file to main memory
+//@input filepath - the path to search for binary file
+//@output - indication if file was found.
+
 bool ISAProgram::readFromFile(const char * filepath){
   FILE *fp;
   unsigned int * buffer;
@@ -54,7 +58,7 @@ bool ISAProgram::readFromFile(const char * filepath){
   fseek(fp, 0, SEEK_SET);
 
   length_ = fileSize/4;
-  buffer = new unsigned int[length_];
+  buffer = new unsigned int[length_]; //Allocate buffer
 
   fread(buffer, sizeof(buffer), length_, fp);
   fclose(fp);
@@ -64,11 +68,19 @@ bool ISAProgram::readFromFile(const char * filepath){
     save(buffer[i], i*4, 4);
   }
 
+  //Reclaim memory
   delete[] buffer;
   buffer=NULL;
 
+  //File was read
   return true;
 }
+
+/* Dumps the registry content at current state to file
+* @input filepath - where to place file
+* @output - indication if write was successful
+*/
+
 
 bool ISAProgram::writeToFile(const char * filepath){
   std::ofstream outfile(filepath, std::ios::binary);
@@ -78,8 +90,9 @@ bool ISAProgram::writeToFile(const char * filepath){
 }
 
 
-/*
-* Function to step through program
+/* Step through the program by decoding and executing next instruction pointed to
+* by pc_. This function contain the implementation of the RISC-V 32RVI instruction subset
+* For different subset/functionality this function must be overwritten.
 */
 
 void ISAProgram::step(){
@@ -87,7 +100,7 @@ void ISAProgram::step(){
   //Get fields
   int instr = 0;
   load(instr, 4*pc_, 4);
-  printAsHex(instr);
+  printAsHex(instr); //REMOVE
   int opcode = instr & 0x7f;
   int funct3 = (instr>>12) & 0x7;
   int imm = (instr >> 20);
@@ -109,10 +122,7 @@ void ISAProgram::step(){
         break;
         case 0x2 : //lW
           std::cout << "lw x" << rd << " " << imm << "(x" << rs1 << ")" << '\n';
-          //std::cout << "Immediate field" << imm << '\n'; //REMOVE
-          //std::cout << "Loading into register no" << rd << "from address:" << registers_[rs1] + imm << '\n'; //REMVOE
           load(registers_[rd], registers_[rs1]+imm, 4);
-          //std::cout << "Didnt colapse" << '\n'; //REMOVE
         break;
         case 0x4 : //lbu
           std::cout << "lbu x" << rd << " " << imm << "(x" << rs1 << ")" << '\n';
@@ -144,7 +154,6 @@ void ISAProgram::step(){
           if(registers_[rs1] < ((unsigned int) (imm & 0xfff) )){registers_[rd] = 1;}
           else {registers_[rd] = 0;}
           break;
-          break;
         case 0x4:
           std::cout << "xori x" << rd << " x" << rs1 << " " << imm <<  '\n';
           registers_[rd] = registers_[rs1] ^ imm;
@@ -155,7 +164,6 @@ void ISAProgram::step(){
             registers_[rd] = registers_[rs1] >> imm;
           } else {
             std::cout << "srai x" << rd << " x" << rs1 << " " << (imm & 0x1f) <<  '\n';
-            //printAsHex(registers_[rs1]);
             registers_[rd] = registers_[rs1] >> (imm & 0x1f);
           }
           break;
@@ -170,7 +178,6 @@ void ISAProgram::step(){
         } break;
     case 0x17 : //auipc
       imm = ((instr >> 12) << 12);
-
       std::cout << "auipc x" << rd << " " << imm << '\n';
       registers_[rd] = pc_*4+imm;
       break;
@@ -237,9 +244,7 @@ void ISAProgram::step(){
           std::cout << "AND x" << rd << " x" << rs1 << " x" << rs2 <<  '\n';
           registers_[rd] = registers_[rs1] & registers_[rs2];
           break;
-
       }
-
       break;
     case 0x37 :
       std::cout << "lui x" << rd << " " << ((instr >> 12) & 0xfffff) << '\n';
@@ -295,14 +300,15 @@ void ISAProgram::step(){
       ecall_ = true;
       break;
   }
-
-  registers_[0] = 0;
-
+  registers_[0] = 0; //jalr x0 xX is not supposed to cast exception, so this is the easier way
   pc_++; //Increment program counter
 }
 
+/* Return indication if program is ended
+*/
+
 bool ISAProgram::hasNext(){
-  return ((pc_ < length_) & !ecall_) ;
+  return ((pc_ < length_) & !ecall_);
 }
 
 void ISAProgram::printRegisters(){
@@ -310,12 +316,6 @@ void ISAProgram::printRegisters(){
   for(int i=0; i<32; i++){
     std::cout << "x" << i << " " << registers_[i] << std::endl;
   }
-}
-
-/* Function that prints out data stored on stack
-*/
-void ISAProgram::printMemory(){
-  //Nothing
 }
 
 
@@ -329,14 +329,27 @@ void ISAProgram::printProgram(){
   }
 }
 
-//Helper methods
 
-bool ISAProgram::save(int w, unsigned int sp, unsigned int bytes){
+/*
+* Saves 1-4 bytes from the integer w to memory
+* Byte ordering is little endian format
+* @input w - integer to be saved
+* @input idx - memory idx to save least sig. byte to
+* @input bytes - number of bytes to save
+*/
+bool ISAProgram::save(int w, unsigned int idx, unsigned int bytes){
   for(unsigned int i=0; i < bytes; i++){
-    memory_[sp+i] = (unsigned char) (w>>(8*i)) & 0xff;
+    memory_[idx+i] = (unsigned char) (w>>(8*i)) & 0xff;
   }
   return true;
 }
+
+/* Single function to load from memory. Is
+* @input &w - register to place content passed by reference
+* @input sp - pointer to location of least significant byte (little endian format)
+* @input
+*/
+
 
 bool ISAProgram::load(int &w, unsigned int sp, unsigned int bytes, bool u){
   w = 0;
@@ -345,7 +358,7 @@ bool ISAProgram::load(int &w, unsigned int sp, unsigned int bytes, bool u){
   }
 
   if(u != true){
-    if((memory_[sp+bytes-1] >> 7) == 1){
+    if((memory_[sp+bytes-1] >> 7) == 1){ //For sign extension
       for(int i=4; i>bytes; i--){
         w = w | 0xff << (8*(i-1));
       }
@@ -353,6 +366,10 @@ bool ISAProgram::load(int &w, unsigned int sp, unsigned int bytes, bool u){
   }
   return true;
 }
+
+/* Test function used to print content of memory
+*
+*/
 
 void ISAProgram::printAsHex(unsigned int instr){
   int res = 0;
--- isaprogram.h
+++ isaprogram.h
@@ -16,24 +16,25 @@ class ISAProgram {
 
     bool readFromFile(const char * filepath);
     bool writeToFile(const char * filepath);
-    void step();
 
+    void step();
     bool hasNext();
+
     void printRegisters();
     void printProgram();
-    void printMemory();
 
   private:
-    int length_; //Program length
+    int length_; //Program length in number of instructions
     int pc_; //Program counter
     int registers_[32]; //
     bool ecall_; //For program termination
 
+    //capacity currently not used
     unsigned int capacity_; //Memory capacity
     unsigned char * memory_; //Main memory
 
     //Internal functions
-    bool save(int w, unsigned int sp, unsigned int bytes);
+    bool save(int w, unsigned int idx, unsigned int bytes);
     bool load(int &w, unsigned int sp, unsigned int bytes, bool u=false);
     void printAsHex(unsigned int instr);
 };
